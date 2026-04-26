10f131d73a23a4667c77dad377d455cb106d026a
Nipuni-Liyanage <liyanagenipuni25@gmail.com>
2026-04-26 23:57:56 +0530

    Hide floating cart button on login page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import FloatingCartButton from './components/layout/FloatingCartButton.vue'
+
+const route = useRoute()
+
+const showFloatingCart = computed(() => route.path !== '/login')
 </script>
 
 <template>
   <router-view />
-  <FloatingCartButton />
+  <FloatingCartButton v-if="showFloatingCart" />
 </template>
